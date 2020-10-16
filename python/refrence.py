f19598225e999e46d3e0e69c2c43dd9900b73499
reza <rezabojnordi2012@gmail.com>
2020-10-16 22:10:56 +0330

    garbet collection

---
+++ python/refrence.py
@@ -0,0 +1,19 @@
+import sys
+import ctypes
+a = [1,2,3,4]
+
+print(id(a))
+
+print(sys.getrefcount(a))
+
+
+def ref_count(address:int):
+    return ctypes.c_long.from_address(address).value
+
+print(ref_count(id(a)))
+
+b = a
+
+print(ref_count(id(b)))
+
+
