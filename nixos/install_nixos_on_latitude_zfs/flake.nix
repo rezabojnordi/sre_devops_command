ce239da72a485ddb90281d32368b6552f2981039
Reza Bojnordi <rezabojnordi2012@gmail.com>
2026-02-17 09:37:16 +0300

    Create flake.nix

---
+++ nixos/install_nixos_on_latitude_zfs/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, disko, ... }: {
+    nixosConfigurations.server2 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./disko-config.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
